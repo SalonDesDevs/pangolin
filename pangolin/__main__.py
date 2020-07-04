17e1d75a329a78c628692cefdd27d8fcc1765321
Julien Castiaux <Julien.castiaux@gmail.com>
2020-07-04 11:44:33 +0200

    cli and logging config

--- pangolin/__main__.py
+++ pangolin/__main__.py
@@ -1,4 +1,21 @@
-from game import Game
+import argparse
+import logging
 
-if __name__ == "__main__":
-    Game().start()
+# cli options
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="count", help="increase verbosity", default=0)
+parser.add_argument("-s", "--silent", action="count", help="decrease verbosity", default=0)
+options = parser.parse_args()
+verbosity = 10 * max(0, 3 + options.verbose - options.silent, 5)
+
+# logging configuration
+stdout = logging.StreamHandler()
+stdout.formatter = logging.Formatter(
+    "{asctime} [{levelname}] <{name}:{funcName}> {message}", style="{")
+logging.root.handlers.clear()
+logging.root.addHandler(stdout)
+logging.root.setLevel(verbosity)
+
+from pangolin.game import Game
+game = Game()
+game.start()
