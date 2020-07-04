ac21c9240853e71915b54352a3cf446ec8f46b7f
Julien Castiaux <Julien.castiaux@gmail.com>
2020-07-04 15:36:00 +0200

    more maths

--- pangolin/entities.py
+++ pangolin/entities.py
@@ -1,17 +1,29 @@
 from geometry import Vector
 import pygame
+import logging
+
+logger = logging.getLogger(__name__)
+
+SPEED = 1
+
+UP = SPEED * Vector.from_xy(0, 1)
+RIGHT = SPEED * Vector.from_xy(1, 0)
+UP_RIGHT = SPEED * Vector.from_xy(1, 1)
+UP_LEFT = SPEED * Vector.from_xy(-1, 1)
+DOWN = -UP
+LEFT = -RIGHT
+DOWN_LEFT = -UP_RIGHT
+DOWN_RIGHT = -UP_LEFT
 
 
 class Bubble:
-    x: int
-    y: int
+    pos: Vector
     vel: Vector
     acc: Vector
     size: int
 
-    def __init__(self, x: int, y: int, vel: Vector, acc: Vector, size: int):
-        self._x = x
-        self._y = y
+    def __init__(self, pos: Vector, vel: Vector, acc: Vector, mass: int):
+        self.pos = pos
         self.vel = vel
         self.acc = acc
         self._size = size
@@ -26,23 +38,21 @@ class Bubble:
 
     @property
     def x(self):
-        return self._x
+        return self.pos.x
 
     @property
     def y(self):
-        return self._y
+        return self.pos.y
 
     def move(self):
+        self.vel = self.vel.transform(self.acc)
+        self.pos = self.pos.transform(self.vel)
+
+    def collide(self, other: Bubble):
         pass
 
 
 class BubbleEntity(Bubble):
-    x: int
-    y: int
-    vel: Vector
-    acc: Vector
-    size: int
-
     color: tuple
 
     def __init__(
@@ -57,3 +67,15 @@ class BubbleEntity(Bubble):
 
     def draw(self, screen, scale):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size * scale)
+
+
+class Player(Bubble):
+    K = 10
+
+    def move(self):
+        cls = type(self)
+        friction = -cls.K * Vector.from_xy(self.vel.x, self.vel.y)
+        self.acc = self.acc.transform(friction)
+        super().move()
+
+
--- pangolin/geometry.py
+++ pangolin/geometry.py
@@ -1,19 +1,24 @@
 from math import *
 from typing import NamedTuple
-from operator import itemgetter
 
 class Vector(NamedTuple):
     angle: float
     norm: float
 
     def transform(self, other: "Vector"):
-        sx = self.norm * cos(self.angle)
-        sy = self.norm * sin(self.angle)
+        return self.from_xy(self.x + other.x, self.y + other.y)
 
-        ox = other.norm * cos(other.angle)
-        oy = other.norm * sin(other.angle)
+    @property
+    def x(self):
+        return self.norm * cos(self.angle)
 
-        nx = sx + ox
-        ny = sy + oy
+    @property
+    def y(self):
+        return self.norm * sin(self.angle)
 
-        return type(self)()
+    @classmethod
+    def from_xy(cls, x, y):
+        return cls(sqrt(x ** 2 + y ** 2), atan2(y, x))
+
+    def __mul__(self, x):
+        return type(self).from_xy(self.x * x, self.y * x)
