dbf11adb87a576a6b941e7b92bc3e27be5ab1457
Thomas Marchand <thomas.marchand@tuta.io>
2020-07-04 11:45:44 +0200

    initial commit for bubbles

--- pangolin/bubbles.py
+++ pangolin/bubbles.py
@@ -1,5 +1,26 @@
 class Bubble:
-    pass
+    def __init__(self):
+        self.spawn()
 
-class Player:
-    pass
+    def spawn(self):
+        pass
+
+    def move(self, delay):
+        pass
+
+
+class Player(Bubble):
+    def deviate_up(self, delay):
+        self.deviate(0, delay)
+
+    def deviate_down(self, delay):
+        self.deviate(0, -delay)
+
+    def deviate_right(self, delay):
+        self.deviate(delay, 0)
+
+    def deviate_left(self, delay):
+        self.deviate(-delay, 0)
+
+    def deviate(self, x, y):
+        pass
