7c4d21c4da46023da97b4a7743ec6bce3ce5561f
Thomas Marchand <thomas.marchand@tuta.io>
2020-07-04 11:08:58 +0200

    add default pygame

--- pangolin/game.py
+++ pangolin/game.py
@@ -1,3 +1,24 @@
+import pygale
+
+
 class Game:
-    def start():
+    def update(self, delay):
         pass
+
+    def draw(self, screen):
+        pass
+
+    def start_pygame(self):
+        pygame.init()
+        fps = 60.0
+        fpsClock = pygame.time.Clock()
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        delay = 1 / fps  # delay is the time since last frame.
+        while True:
+            self.update(delay)
+            self.draw(screen)
+            delay = fpsClock.tick(fps)
+
+    def start():
+        self.start_pygame()
